e26279dd44b08b80e175c423d388cf1a4c6e147e
rohitsch1 <97652690+rohitsch1@users.noreply.github.com>
2022-08-08 22:36:46 +0530

    get API assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,44 @@
 const express = require('express');
-const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
-
-    res.send('My second ever api!')
-});
-
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+// question 1 
+router.get('/movies', function (req, res){
+    let movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies)
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+router.get('/movies/:indexNumber', function(req, res){
     
-    res.send('Dummy response')
+    let requestParams = req.params.indexNumber
+    let movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let len = movies.length;
+    let result =movies[requestParams];
+    if (requestParams > len){
+       return res.send("invalid index is given, please change the index .")
+    }else{
+       return res.send(result)
+
+    }
 })
+
+router.get('/films', function (req, res) {
+   let ArrayOfMovies=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+    
+
+    res.send({key: ArrayOfMovies})
+});
 
 module.exports = router;
